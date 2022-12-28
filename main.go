191cb92b614ddda4216bf06e80ad69b4ab5a2520
aditiaz <aditiaan96a@gmail.com>
2022-12-28 21:34:23 +0700

    added a function to redirect to home

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -19,6 +20,7 @@ func main() {
 	route.PathPrefix("/public/").Handler(http.StripPrefix("/public/",http.FileServer(http.Dir("./public"))))
 	
 	route.HandleFunc("/home",home).Methods("GET")
+	route.HandleFunc("/home",addMyProject).Methods("POST")
 	route.HandleFunc("/contactMe",contactMe).Methods("GET")
 	route.HandleFunc("/addProject",addProject).Methods("GET")
 	route.HandleFunc("/detailProject",detailProject).Methods("GET")
@@ -40,6 +42,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	tmpl.Execute(w,Data)
 }
+func addMyProject(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Title : " + r.PostForm.Get("name"))
+	fmt.Println("Start Date : " + r.PostForm.Get("start-date"))
+	fmt.Println("End Date : " + r.PostForm.Get("end-date"))
+	fmt.Println("Description : " + r.PostForm.Get("description"))
+	// fmt.Println("Technologies : " + r.PostForm.Get("checkbox"))
+	http.Redirect(w,r,"/home",http.StatusMovedPermanently)
+}
+
 func contactMe(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
