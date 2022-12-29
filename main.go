2d1354d4f70c2cda41cdd87f9ab684bb0dceb6fb
aditiaz <aditiaan96a@gmail.com>
2022-12-29 22:24:41 +0700

    what's done : detail page, icons for each card and add new card

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -14,6 +15,35 @@ var Data = map[string]interface{}{
 	"IsLogin": true,
 }
 
+type dataInput struct{
+	// Id int
+	ProjectName string
+	Description string
+	Technologies []string
+	// StartDate string
+	// EndDate string
+	Duration string
+	Image string
+	
+
+
+}
+
+var dataInputs = []dataInput{
+	{
+		// Id: 1
+		ProjectName:"Pasar Coding di Indonesia Dinilai Masih Menjanjikan",
+		Description: "Deskripsi",
+		Technologies:[]string {"nodejs","golang","reactjs","python"},
+		// StartDate: "2022-12-11",
+		// EndDate: "2022-12-31",
+		Duration: "3 Months",
+		Image:   "/public/assets/female-portrait.jpg",
+		
+	},
+}
+
+
 func main() {
 	route := mux.NewRouter()
 
@@ -21,9 +51,11 @@ func main() {
 	
 	route.HandleFunc("/home",home).Methods("GET")
 	route.HandleFunc("/home",addMyProject).Methods("POST")
+	route.HandleFunc("/projectDetail/{id}",projectDetail).Methods("GET")
 	route.HandleFunc("/contactMe",contactMe).Methods("GET")
 	route.HandleFunc("/addProject",addProject).Methods("GET")
 	route.HandleFunc("/detailProject",detailProject).Methods("GET")
+	route.HandleFunc("/delete-Project/{id}", deleteProject).Methods("GET")
 
 	fmt.Println("Server is running on port 5000")
 	http.ListenAndServe("localhost:5000", route)
@@ -38,22 +70,71 @@ func home(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("message :" + err.Error()))
 		return
 	}
-
+	respData := map[string]interface{}{
+		// "Data":  Data,
+		"dataInputs": dataInputs,
+	}
 	w.WriteHeader(http.StatusOK)
-	tmpl.Execute(w,Data)
+	tmpl.Execute(w,respData)
 }
+
 func addMyProject(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Title : " + r.PostForm.Get("name"))
-	fmt.Println("Start Date : " + r.PostForm.Get("start-date"))
-	fmt.Println("End Date : " + r.PostForm.Get("end-date"))
-	fmt.Println("Description : " + r.PostForm.Get("description"))
-	fmt.Println("Technologies : " + r.PostForm.Get("checkbox"))
+
+    projectName :=  r.PostForm.Get("name")
+	// startDate := r.PostForm.Get("start-date")
+	// endDate := r.PostForm.Get("end-date")
+	// duration := endDate - startDate
+	descrition := r.PostForm.Get("description")
+	checkbox := r.Form["checkbox"]
+	image := r.PostForm.Get("image")
+	var newProject = dataInput {
+		ProjectName: projectName,
+	    Description: descrition,
+	    Technologies: checkbox,
+        Duration: "3 Months",
+	    Image: image,
+	
+	}
+
+	dataInputs = append(dataInputs, newProject)
+	fmt.Println(dataInputs)
 	http.Redirect(w,r,"/home",http.StatusMovedPermanently)
+}
+
+func projectDetail( w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+
+	id,_ := strconv.Atoi(mux.Vars(r)["id"])
+
+	var tmpl,err = template.ParseFiles("views/detail-page.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message :" + err.Error()))
+		return
+	}
+	resp := map[string]interface{}{
+		"dataInputs" : dataInputs[id],
+	}
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w,resp)
+
+}
+
+func deleteProject(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	fmt.Println(id)
+
+	dataInputs = append(dataInputs[:id], dataInputs[id+1:]...)
+
+	http.Redirect(w, r, "/home", http.StatusMovedPermanently)
 }
 
 func contactMe(w http.ResponseWriter, r *http.Request) {
